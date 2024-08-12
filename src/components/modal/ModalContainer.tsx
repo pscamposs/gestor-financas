ea520d69ddfa1538209f8cfaad31830ff8635b02
pscamposs <pscampos221@gmail.com>
2024-08-11 22:16:42 -0300

    feat: Custom modal element

---
+++ src/components/modal/ModalContainer.tsx
@@ -0,0 +1,13 @@
+import useModal from "@/hook/use-modal-context";
+
+export default function ModalContainer() {
+  const { isOpen, modal } = useModal();
+
+  if (isOpen) {
+    return (
+      <div className="flex justify-center items-center absolute top-0 left-0 w-dvw h-dvh bg-zinc-500/35 z-50 px-8">
+        {modal}
+      </div>
+    );
+  }
+}
