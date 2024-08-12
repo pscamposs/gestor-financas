6b6e200d0a69f8d610e50c078c74c59244ea8b44
pscamposs <pscampos221@gmail.com>
2024-08-11 22:16:04 -0300

    feat: dashboard invoice chart

--- src/components/charts/InvoiceChart.tsx
+++ src/components/charts/InvoiceChart.tsx
@@ -73,14 +73,55 @@ const data = [
   },
 ];
 
-export default function InvoiceChart() {
+const getMetrics = (data: InvoiceProps[]) => {
+  const metrics: { name: string; Entrada: number; Saida: number }[] = [];
+  const monthNames = [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Abr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Set",
+    "Out",
+    "Nov",
+    "Dez",
+  ];
+
+  data.forEach((invoice) => {
+    const invoiceDate = new Date(invoice.date);
+    const month = invoiceDate.getMonth();
+
+    let metric = metrics.find((m) => m.name === monthNames[month]);
+    if (!metric) {
+      metric = { name: monthNames[month], Entrada: 0, Saida: 0 };
+      metrics.push(metric);
+    }
+
+    if (invoice.flow === "Entrada") {
+      metric.Entrada += Number(invoice.value) / 100;
+    } else if (invoice.flow === "Saída") {
+      metric.Saida += Number(invoice.value) / 100;
+    }
+  });
+
+  metrics.sort(
+    (a, b) => monthNames.indexOf(a.name) - monthNames.indexOf(b.name)
+  );
+
+  return metrics;
+};
+
+export default function InvoiceChart({ data }: { data: InvoiceProps[] }) {
   return (
     <div className="p-8 inline-block mt-8 cursor-default">
       <h3 className="text-zinc-600 font-bold">Métricas Mensais</h3>
       <BarChart
-        width={500}
+        width={1000}
         height={400}
-        data={data}
+        data={getMetrics(data)}
         margin={{
           top: 16,
           right: 30,
