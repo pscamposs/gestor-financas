5590942bc67955f2e21599468a9d6cdb5142b6a7
pscamposs <pscampos221@gmail.com>
2024-08-11 22:12:12 -0300

    feat: Context provider for Modal and QueryClient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,24 @@
 import Sidebar from "@/components/Sidebar";
 import { useState } from "react";
 import Dashboard from "./dashboard/page";
+import ModalContainer from "@/components/modal/ModalContainer";
+import { ModalContextProvider } from "@/context/useModal";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function Home() {
   const [viewPage, setViewPage] = useState<React.ReactElement>(<Dashboard />);
 
   return (
-    <main className="flex overflow-hidden">
-      <Sidebar setView={setViewPage} />
-      <section className="flex-1 p-4 w-dvw">{viewPage}</section>
+    <main className="flex overflow-hidden relative">
+      <QueryClientProvider client={queryClient}>
+        <ModalContextProvider>
+          <Sidebar setView={setViewPage} />
+          <section className="flex-1 p-4 w-dvw">{viewPage}</section>
+          <ModalContainer />
+        </ModalContextProvider>
+      </QueryClientProvider>
     </main>
   );
 }
