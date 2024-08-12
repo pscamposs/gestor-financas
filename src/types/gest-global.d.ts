aebcaf0adbb538d9405f45ecbad9c22ae21a9db2
pscamposs <pscampos221@gmail.com>
2024-08-11 22:18:03 -0300

    feat: application types

---
+++ src/types/gest-global.d.ts
@@ -0,0 +1,55 @@
+interface ProfileProps {
+  id: number;
+  name: string;
+  salary: number;
+}
+
+interface InvoiceProps {
+  id: number;
+  name: string;
+  description: string;
+  value: number;
+  method: string;
+  installments: number;
+  flow: string;
+  date: string;
+  bank_label: string;
+  bank_color: string;
+  bank_icon: string;
+  bank_closing_day: number;
+}
+
+interface DashboardProps {
+  profile: ProfileProps;
+  invoices: InvoiceProps[];
+}
+
+interface BankProps {
+  id: number;
+  label: string;
+  color: string;
+  icon: string;
+  invoice_closing_day: number;
+}
+
+interface SelectorProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  options: OptionProps[];
+  onChangeHandler?: (value: OptionProps[]) => void;
+}
+
+interface SelectorListItemProps {
+  option: OptionProps;
+  onClick: (option: OptionProps) => void;
+}
+
+interface OptionProps {
+  id?: number;
+  icon?: string;
+  label: string;
+}
+
+interface SelectorItemProps {
+  option: OptionProps;
+  onClick: (option: OptionProps) => void;
+}
