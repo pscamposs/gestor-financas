1a25672369efdda749d12d393ce8e69176b504b9
pscamposs <pscampos221@gmail.com>
2024-08-11 22:16:27 -0300

    feat: Custom modal for bank creation

---
+++ src/components/modal/BankModal.tsx
@@ -0,0 +1,124 @@
+import { isValidSvg } from "@/utils/FormatterUtils";
+import { Input } from "../Input";
+import { TextArea } from "../TextArea";
+import useModal from "@/hook/use-modal-context";
+import { useState } from "react";
+import Button from "../Button";
+
+interface InvoiceModalProps {
+  data?: BankProps;
+  refetch: () => void;
+}
+
+export default function BankModal({ data, refetch }: InvoiceModalProps) {
+  let { closeModal } = useModal();
+
+  let [formData, setFormData] = useState<BankProps | any>(data);
+  let [isSvgValid, setIsSvgValid] = useState<boolean>(true);
+
+  async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (data) {
+      setFormData((prevFormData: InvoiceProps) => ({
+        ...prevFormData,
+      }));
+    }
+    if (!isSvgValid) {
+      alert("Corrija o icone do banco.");
+      return;
+    }
+
+    const response = await fetch("/api/bank", {
+      method: data ? "PUT" : "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "Application/Json",
+      },
+    });
+
+    if (response.status == 200) {
+      refetch();
+      closeModal();
+    }
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<any>) => {
+    const { name, value } = e.target;
+    if (name === "image") {
+      setIsSvgValid(isValidSvg(value));
+    }
+    setFormData((prev: any) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="bg-white p-8">
+      <h3 className="text-zinc-500 ">{data ? "Editar" : "Registrar"} Banco</h3>
+      <form onSubmit={onSubmit}>
+        <div className="flex gap-2 flex-wrap">
+          <Input
+            label="Nome"
+            required
+            name="label"
+            value={formData?.label}
+            onChange={handleInputChange}
+          />
+          <Input
+            label="Dia do fechamento"
+            type="number"
+            required
+            description="Fatura"
+            name="invoice_closing_day"
+            value={formData?.invoice_closing_day}
+            onChange={handleInputChange}
+            max={31}
+            min={1}
+          />
+        </div>
+        <div>
+          <Input
+            label="Cor do banco"
+            name="color"
+            type="color"
+            value={formData?.color}
+            onChange={handleInputChange}
+            required
+          />
+          <TextArea
+            label="Icone do banco"
+            name="icon"
+            description="Svg"
+            value={formData?.icon}
+            onChange={handleInputChange}
+            required
+          />
+          {isSvgValid ? (
+            <div className="py-2">
+              <p className="text-zinc-300 font-light py-2">Preview:</p>
+              <div
+                dangerouslySetInnerHTML={{ __html: formData?.icon }}
+                className="w-7 "
+              ></div>
+            </div>
+          ) : (
+            <p className="text-red-500 py-1">Svg inválido</p>
+          )}
+        </div>
+        <div className="my-2">
+          <button className="bg-sky-500 text-zinc-50 px-4 py-2">
+            Finalizar
+          </button>
+          <button
+            className="text-red-500 px-4 py-2"
+            onClick={() => closeModal()}
+          >
+            Cancelar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
