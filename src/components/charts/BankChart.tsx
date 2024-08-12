df699b040b1f24dcc8bac349a57756e3d8a77524
pscamposs <pscampos221@gmail.com>
2024-08-11 22:15:52 -0300

    feat: dashboard bank chart

--- src/components/charts/BankChart.tsx
+++ src/components/charts/BankChart.tsx
@@ -1,34 +1,8 @@
-import React from "react";
+import { formatCurrency } from "@/utils/FormatterUtils";
+import React, { useEffect, useState } from "react";
 
-interface BankMetricProps {
-  color: string;
-  title: string;
-  total: number;
-}
-
-const data: BankMetricProps[] = [
-  {
-    title: "Nubank",
-    total: 1242.24,
-    color: "#C4B5FD",
-  },
-  {
-    title: "Inter",
-    total: 524.9,
-    color: "#FDBA74",
-  },
-  {
-    title: "Itaú",
-    total: 192.12,
-    color: "#FCD34D",
-  },
-];
-
-let calcProgress = (bank: BankMetricProps) => {
-  let total = data.reduce((acc, current) => acc + current.total, 0);
-  let percent = Math.round((bank.total / total) * 100);
-  console.log(percent);
-  return percent;
+const calcProgress = (bankTotal: number, total: number) => {
+  return total === 0 ? 0 : Math.round((bankTotal / total) * 100);
 };
 
 const ProgressBar = ({
@@ -48,41 +22,87 @@ const ProgressBar = ({
   );
 };
 
-const BankMetric: React.FC<{ bank: BankMetricProps }> = ({ bank }) => {
+const BankMetric: React.FC<{
+  bank: string;
+  total: number;
+  overallTotal: number;
+  banks: InvoiceProps[];
+}> = ({ bank, total, overallTotal, banks }) => {
+  const getBankColor = (bank: string) => {
+    return banks.find((b) => b.bank_label === bank)?.bank_color || "#000";
+  };
+
   return (
     <div className="py-2">
       <div className="flex justify-between">
-        <h2 className={`text-sm font-semibold`} style={{ color: bank.color }}>
-          {bank.title}
+        <h2
+          className="text-sm font-semibold"
+          style={{ color: getBankColor(bank) }}
+        >
+          {bank}
         </h2>
-        <h3 className="text-red-300 text-xl font-bold">{bank.total}</h3>
+        <h3 className="text-red-300 text-xl font-bold">
+          {formatCurrency(total)}
+        </h3>
       </div>
       <div className="relative">
-        <ProgressBar progress={calcProgress(bank)} color={bank.color} />
+        <ProgressBar
+          progress={calcProgress(total, overallTotal)}
+          color={getBankColor(bank)}
+        />
       </div>
     </div>
   );
 };
 
-export default function BankChart() {
+export default function BankChart({ data }: { data: InvoiceProps[] }) {
+  const [banks, setBanks] = useState<Record<string, number>>({});
+  const [banksTotal, setBanksTotal] = useState(0);
+
+  useEffect(() => {
+    const totals = data.reduce((acc, invoice) => {
+      console.log(invoice);
+      const { bank_label, value, flow } = invoice;
+
+      if (flow != "Entrada") {
+        const numericValue = Number(value);
+
+        if (!isNaN(numericValue)) {
+          acc[bank_label] = (acc[bank_label] || 0) + numericValue;
+        }
+      }
+
+      return acc;
+    }, {} as Record<string, number>);
+
+    const total = Object.values(totals).reduce(
+      (sum, bankTotal) => sum + bankTotal,
+      0
+    );
+
+    setBanks(totals);
+    setBanksTotal(total);
+    console.log(totals, total);
+  }, [data]);
+
   return (
-    <div className="p-8 inline-block mt-8 w-96 cursor-default ">
+    <div className="p-8 inline-block mt-8 w-96 cursor-default">
       <h3 className="text-zinc-600 font-bold">Gastos Bancários</h3>
       <div>
-        {data.map((bank, index) => (
-          <BankMetric key={index} bank={bank} />
+        {Object.entries(banks).map(([bank, total]) => (
+          <BankMetric
+            key={bank}
+            bank={bank}
+            banks={data}
+            total={total}
+            overallTotal={banksTotal}
+          />
         ))}
       </div>
       <hr className="my-4" />
       <div>
         <h2 className="text-zinc-600 font-bold">Total</h2>
-        <p className="text-red-300">
-          {(
-            Math.round(
-              data.reduce((acc, current) => acc + current.total, 0) * 100
-            ) / 100
-          ).toFixed(2)}
-        </p>
+        <p className="text-red-300">{formatCurrency(banksTotal)}</p>
       </div>
     </div>
   );
