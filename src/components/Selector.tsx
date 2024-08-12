eb54bb567ee1fb52b44eec14889d199d665093d4
pscamposs <pscampos221@gmail.com>
2024-08-11 22:15:15 -0300

    feat: Custom selector

---
+++ src/components/Selector.tsx
@@ -0,0 +1,148 @@
+// Selector.tsx
+import { X } from "lucide-react";
+import { useState, useRef, useEffect } from "react";
+
+const SelectorListItem = ({ onClick, option }: SelectorListItemProps) => {
+  return (
+    <li
+      className="cursor-pointer p-2 bg-zinc-50 hover:bg-zinc-200 transition-all"
+      onClick={() => onClick(option)}
+    >
+      <div className="flex items-center gap-2">
+        {option.icon && (
+          <div
+            dangerouslySetInnerHTML={{ __html: option.icon as string }}
+            className="w-6"
+          ></div>
+        )}
+        {option.label}
+      </div>
+    </li>
+  );
+};
+
+const SelectorItem = ({ onClick, option }: SelectorItemProps) => {
+  return (
+    <button
+      className="flex bg-sky-200 text-xs items-center gap-1 p-1.5 hover:bg-sky-100 transition-all"
+      onClick={() => onClick(option)}
+    >
+      <div className="flex items-center gap-2">
+        {option.icon && (
+          <div
+            dangerouslySetInnerHTML={{ __html: option.icon as string }}
+            className="w-6"
+          ></div>
+        )}
+        {option.label}
+      </div>
+      <X size={12} />
+    </button>
+  );
+};
+
+export const Selector: React.FC<SelectorProps> = ({
+  label,
+  options,
+  onChangeHandler,
+  ...props
+}: SelectorProps) => {
+  const [selectedItems, setSelectedItems] = useState<OptionProps[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [filteredOptions, setFilteredOptions] =
+    useState<OptionProps[]>(options);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSelectItem = (option: OptionProps) => {
+    setIsOpen(false);
+    if (selectedItems.some((item) => item.label === option.label)) return;
+    const newSelectedItems = [...selectedItems, option];
+    setSelectedItems(newSelectedItems);
+    if (inputRef.current) {
+      inputRef.current.value = option.label;
+    }
+    if (onChangeHandler) {
+      onChangeHandler(newSelectedItems);
+    }
+  };
+
+  const handleDocumentClick = (event: MouseEvent) => {
+    if (
+      containerRef.current &&
+      !containerRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleDocumentClick);
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, []);
+
+  const handleRemoveItem = (option: OptionProps) => {
+    const newSelectedItems = selectedItems.filter(
+      (item) => item.label !== option.label
+    );
+    setSelectedItems(newSelectedItems);
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+    setFilteredOptions(options);
+    if (onChangeHandler) {
+      onChangeHandler(newSelectedItems);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value.toLowerCase();
+    setFilteredOptions(
+      options.filter((option) => option.label.toLowerCase().includes(value))
+    );
+    setIsOpen(true);
+  };
+
+  return (
+    <div className="text-sm" ref={containerRef}>
+      <label htmlFor={label} className="block text-zinc-600 my-0.5 rounded">
+        {label} {props.required && <span className="text-red-600">*</span>}
+      </label>
+      <div className="relative bg-zinc-100 outline-sky-500 text-gray-700 pb-1">
+        {selectedItems.length === 0 && (
+          <input
+            {...props}
+            type="text"
+            className="w-full bg-inherit outline-none py-2 px-1.5"
+            onFocus={() => setIsOpen(true)}
+            onChange={handleInputChange}
+            name="bank"
+            ref={inputRef}
+          />
+        )}
+        <div className="flex gap-1 flex-wrap pl-1">
+          {selectedItems.map((item) => (
+            <SelectorItem
+              key={item.label}
+              option={item}
+              onClick={handleRemoveItem}
+            />
+          ))}
+        </div>
+        {isOpen && (
+          <ul className="absolute bg-zinc-100 z-20 w-full my-1.5 rounded overflow-hidden">
+            {filteredOptions.map((option) => (
+              <SelectorListItem
+                key={option.label}
+                option={option}
+                onClick={handleSelectItem}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
