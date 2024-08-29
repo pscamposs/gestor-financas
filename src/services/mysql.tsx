5a4c5cecfe6549ce0bb0f9f18854d028605a49a1
pscamposs <pscampos221@gmail.com>
2024-08-29 17:44:03 -0300

    fix: mysql multiple connection

---
+++ src/services/mysql.tsx
@@ -0,0 +1,29 @@
+import mysql from "mysql2/promise";
+
+let pool: mysql.Pool;
+
+const getConnection = async () => {
+  if (!pool) {
+    pool = mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      database: process.env.DB_DATABASE,
+      password: process.env.DB_PASSWORD,
+    });
+  }
+  return pool.getConnection();
+};
+
+const executeQuery = async (query: string, params?: any[]): Promise<any> => {
+  const connection = await getConnection();
+  try {
+    return await connection.execute(query, params);
+  } catch (error) {
+    console.error("Database query error:", error);
+    throw error;
+  } finally {
+    connection.release();
+  }
+};
+
+export default executeQuery;
