44c71b297a541f4bce29a7931496a110d2e3b58a
pscamposs <pscampos221@gmail.com>
2024-08-29 17:46:15 -0300

    perf: separation of small components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,44 @@
+// Organize os imports
 "use client";
+import { useState, useMemo } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+
 import Sidebar from "@/components/Sidebar";
-import { useState } from "react";
 import Dashboard from "./dashboard/page";
 import ModalContainer from "@/components/modal/ModalContainer";
 import { ModalContextProvider } from "@/context/useModal";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AlertContextProvider } from "@/context/useAlert";
+import { Alerts } from "@/components/Alerts";
+import Private from "@/components/Private";
 
-const queryClient = new QueryClient();
+function AppProviders({ children }: { children: React.ReactNode }) {
+  const queryClient = useMemo(() => new QueryClient({}), []);
+
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <AlertContextProvider>
+          <ModalContextProvider>{children}</ModalContextProvider>
+        </AlertContextProvider>
+      </QueryClientProvider>
+    </SessionProvider>
+  );
+}
 
 export default function Home() {
   const [viewPage, setViewPage] = useState<React.ReactElement>(<Dashboard />);
 
   return (
-    <main className="flex overflow-hidden relative">
-      <QueryClientProvider client={queryClient}>
-        <ModalContextProvider>
+    <AppProviders>
+      <main className="flex overflow-hidden">
+        <Alerts />
+        <Private>
           <Sidebar setView={setViewPage} />
           <section className="flex-1 p-4 w-dvw">{viewPage}</section>
-          <ModalContainer />
-        </ModalContextProvider>
-      </QueryClientProvider>
-    </main>
+        </Private>
+        <ModalContainer />
+      </main>
+    </AppProviders>
   );
 }
