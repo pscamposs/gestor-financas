d994982bf7cd7e37e3f281d69d26af23b4117105
pscamposs <pscampos221@gmail.com>
2024-08-29 17:49:31 -0300

    feat: modal context

--- src/hook/use-modal-context.tsx
+++ src/hook/use-modal-context.tsx
@@ -1,5 +1,4 @@
 import { useContext } from "react";
 import { ModalContext } from "../context/useModal";
 
-const useModal = () => useContext(ModalContext);
-export default useModal;
+export const useModal = () => useContext(ModalContext);
